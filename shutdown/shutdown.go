abd41d7f2cf4a4675aca7884cd6d880f325173ed
alexey <piyanin@gmail.com>
2018-02-01 22:21:02 +0300

    init

---
+++ shutdown/shutdown.go
@@ -0,0 +1,33 @@
+package shutdown
+
+import (
+	"os"
+	"os/signal"
+)
+
+var gateKeeper struct {
+	interrupt chan os.Signal
+	shutdown  chan bool
+}
+
+func Register(callback func()) {
+	go func() {
+		<-gateKeeper.shutdown
+		callback()
+	}()
+}
+
+func Shutdown() {
+	close(gateKeeper.interrupt)
+}
+
+func init() {
+	gateKeeper.interrupt = make(chan os.Signal, 2)
+	gateKeeper.shutdown = make(chan bool)
+
+	signal.Notify(gateKeeper.interrupt, os.Interrupt) // CTRL-C
+	go func() {
+		<-gateKeeper.interrupt
+		close(gateKeeper.shutdown)
+	}()
+}
